14d8fde1d73352343b227494212f53b0f5ed1cc3
alaahamdan03 <alaahamdan03@gmail.com>
2024-12-13 22:12:16 +0100

    update and add main.js and index.html

---
+++ main.js
@@ -0,0 +1,57 @@
+// sum
+
+document.write("<h2> + </h2>");
+
+var num1 = +prompt("first number");
+var num2 = +prompt("second number");
+
+sum = num1 + num2;
+
+document.write("<h2>the sum of plus : " + sum + " </h2>");
+
+// -
+
+document.write("<h2> - </h2>");
+
+var num3 = +prompt("first number");
+var num4 = +prompt("second number");
+
+sum1 = num3 - num4;
+
+document.write("<h2>the sum of subtrackt : " + sum1 + " </h2>");
+
+
+// *
+
+document.write("<h2> * </h2>");
+
+var num5 = +prompt("first number");
+var num6 = +prompt("second number");
+
+sum2 = num5 * num6;
+
+document.write("<h2>the sum of multiple : " + sum2 + " </h2>");
+
+
+// /
+
+document.write("<h2> / </h2>");
+
+var num7 = +prompt("first number");
+var num8 = +prompt("second number");
+
+sum3 = num5 / num6;
+
+document.write("<h2>the sum of divide : " + sum3 + " </h2>");
+
+
+// %
+
+document.write("<h2> % </h2>");
+
+var num9 = +prompt("first number");
+var num10 = +prompt("second number");
+
+sum4 = num9 % num10;
+
+document.write("<h2>the sum of module : " + sum4 + " </h2>");
